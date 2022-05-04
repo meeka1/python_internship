0ef7f47ead654a5653b3ef76baeab978457f75c0
Meeka1 <eshmatovamashkhura@gmail.com>
2022-05-04 20:34:30 +0500

    last fixed commit

--- boilerplateUpdated/settings/settings.py
+++ boilerplateUpdated/settings/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'settings.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'settings.wsgi.application'
 
 
 # Database
