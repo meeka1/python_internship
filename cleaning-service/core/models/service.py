c4e8750fbc3fbe539e4d0b7067547c34aac37a52
Meeka1 <eshmatovamashkhura@gmail.com>
2022-05-17 00:35:35 +0500

    forth commit

---
+++ cleaning-service/core/models/service.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .user import User
+
+
+class Service(models.Model):
+    # id, name, cost, company, user_id (fk)
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=60)
+    cost = models.FloatField()
+    company = models.CharField(max_length=30)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.company
