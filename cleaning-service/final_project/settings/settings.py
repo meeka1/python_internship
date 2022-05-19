658ef9354ba9e2a43d3703ac97c5d441712cd87d
Meeka1 <eshmatovamashkhura@gmail.com>
2022-05-19 13:40:45 +0500

    third commit

--- cleaning-service/final_project/settings/settings.py
+++ cleaning-service/final_project/settings/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,9 +133,6 @@ REST_FRAMEWORK = {
     )
 }
 
-
-from datetime import timedelta
-...
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
