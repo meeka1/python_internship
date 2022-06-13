f925d5549d934a0e0e7c4b209cdb20df74e0716a
Meeka1 <eshmatovamashkhura@gmail.com>
2022-06-13 17:52:45 +0500

    moved celery content to tasks.py

--- cleaning-service/core/tasks.py
+++ cleaning-service/core/tasks.py
@@ -2,10 +2,26 @@ from  celery  import shared_task
 from core.models.user  import User
 from django.utils.crypto import get_random_string
 from  random  import  randint
-
-
 import string
 from  random  import  randint, choice
+
+from __future__ import absolute_import
+import os
+from celery import Celery
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_project.settings.settings')
+app = Celery('final_project')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
 
 def random_with_N_digits(n):
     range_start = 10**(n-1)
